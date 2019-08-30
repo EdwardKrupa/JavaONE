d03209ae0ba84fbe11e8144a383d022ed8a031fe
Edward <krupa.edward@gmail.com>
2019-08-31 01:43:14 +0200

    My first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("TripleX v.1!");
+        System.out.println();
+        System.out.println("Hello!\nWelcome to the game called \"Tripple X!\"");
+        System.out.println();
+
+    }
+}
