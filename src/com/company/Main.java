28a6ea910445a3af5c79bbde2955cb63ad809db2
Edward <krupa.edward@gmail.com>
2019-08-31 02:00:11 +0200

    Code formating for better visual effect (testig github controls)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("TripleX v.1!");
         System.out.println();
-        System.out.println("Hello!\nWelcome to the game called \"Tripple X!\"");
+        System.out.println("Hello!");
+        System.out.println("Welcome to te Tripple X! game");
         System.out.println();
 
     }
