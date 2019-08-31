6f0636f3c37b0f2d2a171450fedaaed2d90224f7
Edward <krupa.edward@gmail.com>
2019-08-31 08:11:09 +0200

    version info updated

--- src/com/company/Methods.java
+++ src/com/company/Methods.java
@@ -1,9 +1,12 @@
 package com.company;
 
+import java.util.Random;
 import java.util.Scanner;
 
-public class Methods
+// Contains all methods for Main.java, for JavaONE Project
+public class Methods // Contains all methods for Main.java, for JavaONE Project
 {
+    // GameIntro prints into only at the start of the program
     public static void GameIntro()
     {
         System.out.println("TripleX v.1.4.5!");
@@ -11,41 +14,47 @@ public class Methods
         System.out.println("Hello!");
         System.out.println("Welcome to te Tripple X! game");
         System.out.println();
-        System.out.println("To Start the game please type \"Start\" and confirm with Enter");
-        System.out.println("To Quit the game please type \"Exit\" and confirm with Enter");
+        System.out.println("To Start the game please type \"Start\" and \"s\" confirm with Enter");
+        System.out.println("To Quit the game please type \"Exit\" or \"q\" and confirm with Enter");
         userDecision();
     }
 
+    // Gets information from the user, to start the game or to exit program (includes wrong input from user)
     public static void userDecision()
     {
+
         Scanner scanner = new Scanner(System.in);
         String userInputGameIntro = scanner.nextLine();
 
-        if (userInputGameIntro.equals("start"))
+        if (userInputGameIntro.equals("start") || userInputGameIntro.equals("s"))
         {
             playTheGame();
-        } else if (userInputGameIntro.equals("exit"))
+
+        }
+        else if (userInputGameIntro.equals("exit") || userInputGameIntro.equals("q"))
         {
             exitTheGame();
-        } else
+        }
+        else
         {
-            System.out.println("Please type start or exit");
+            System.out.println("Please type start/s or exit/q");
             userDecision();
         }
     }
 
 
+    // Method for exiting the program, user should be able to quit at every stage of the game
     public static void exitTheGame()
     {
         System.exit(0);
     }
 
+    // Main method for playing the game
     public static void playTheGame()
     {
         short lvlID = 1;
-        while (lvlID <= 11)
-        {
-
-        }
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(10) - 1;
+        System.out.println(randomNumber);
     }
 }
