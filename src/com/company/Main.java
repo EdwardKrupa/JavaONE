4c9f02913f280ff7b0c4b297763b8d7072b38949
Edward <krupa.edward@gmail.com>
2019-08-31 06:07:04 +0200

    Added new content

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,12 @@
 package com.company;
 
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("TripleX v.1!");
-        System.out.println();
-        System.out.println("Hello!");
-        System.out.println("Welcome to te Tripple X! game");
-        System.out.println();
-
+public class Main
+{
+    public static void main(String[] args)
+    {
+        {
+            Methods.GameIntro();
+            Methods.userDecision();
+        }
     }
 }
---
+++ src/com/company/Methods.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Methods
+{
+    public static void GameIntro()
+    {
+        System.out.println("TripleX v.1!");
+        System.out.println();
+        System.out.println("Hello!");
+        System.out.println("Welcome to te Tripple X! game");
+        System.out.println();
+        System.out.println("To Start the game please type \"Start\" and confirm with Enter");
+        System.out.println("To Quit the game please type \"Exit\" and confirm with Enter");
+        userDecision();
+    }
+
+    public static void userDecision()
+    {
+        Scanner scanner = new Scanner(System.in);
+        String userInputGameIntro = scanner.nextLine();
+
+        if (userInputGameIntro.equals("start"))
+        {
+            playTheGame();
+        } else if (userInputGameIntro.equals("exit"))
+        {
+            exitTheGame();
+        } else
+        {
+            System.out.println("Please type start or exit");
+            userDecision();
+        }
+    }
+
+
+    public static void exitTheGame()
+    {
+        System.exit(0);
+    }
+
+    public static void playTheGame()
+    {
+        short lvlID = 1;
+        while (lvlID <= 11)
+        {
+            
+        }
+    }
+}
